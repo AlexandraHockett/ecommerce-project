6abfc71c37de50982359c26e2bbf282abc1e2f62
AlexandraHockett <alexandramandio@gmail.com>
2024-12-09 12:50:34 +0000

    customize Header component for e-commerce template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
-  ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  extends: [
+    'plugin:@next/next/recommended', // Next.js specific linting rules
+    '@payloadcms', // Payload CMS-specific rules
+    'eslint:recommended', // ESLint recommended rules
+    'plugin:@typescript-eslint/recommended', // TypeScript rules
+    'plugin:prettier/recommended', // Prettier integration
+  ],
+  ignorePatterns: ['**/payload-types.ts'], // Files to ignore
+  plugins: ['prettier', 'simple-import-sort'], // Include Prettier and import sort plugins
   rules: {
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }], // Prettier rules
+    'no-console': 'off', // Allow console logs
+    'simple-import-sort/imports': 'error', // Enforce sorted imports
+    'simple-import-sort/exports': 'error', // Enforce sorted exports
   },
 }
